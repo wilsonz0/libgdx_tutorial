255d729709589b7948bfb3ac94042d1c9d111362
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-06-08 19:18:58 -0400

    added a new feature where the log makes players lose points

--- core/src/com/mygdx/game/tutorialGame.java
+++ core/src/com/mygdx/game/tutorialGame.java
@@ -11,6 +11,8 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
@@ -20,10 +22,15 @@ import com.badlogic.gdx.utils.TimeUtils;
 public class tutorialGame extends Game {
 	private OrthographicCamera camera;			// ??? 
 	private Array<Rectangle> raindrops;
+	private Array<Rectangle> logs;
 	private Rectangle bucket;
+	
 	SpriteBatch batch;
+	ShapeRenderer shapeRenderer;
+	
 	Texture bucketImg;
 	Texture dropletImg;
+	Texture logImg;
 	Sound dropSound;
 	Music rainMusic;
 	BitmapFont font;
@@ -31,14 +38,19 @@ public class tutorialGame extends Game {
 	float SPEED = 240;
 	float x, y;
 	private long lastDropTime;
+	private long lastLogTime;
 	boolean activated = false;
 	private long lastPowerTime;
 	
-	public int score = 100000;
+	public int score = 0;
+	
+	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		shapeRenderer = new ShapeRenderer();
+		
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
 		font = new BitmapFont();
@@ -46,6 +58,20 @@ public class tutorialGame extends Game {
 		// the images		
 		bucketImg = new Texture("bucket.png");
 		dropletImg = new Texture("droplet.png");
+		logImg = new Texture("log2.png");
+		
+		// initializing the images		
+		bucket = new Rectangle();
+	    bucket.x = 800 / 2 - 64 / 2;
+	    bucket.y = 20;
+	    bucket.width = 64;
+	    bucket.height = 64;
+	    
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
+		
+		logs = new Array<Rectangle>();
+		spawnLog();
 		
 		// the sound and music
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
@@ -54,15 +80,6 @@ public class tutorialGame extends Game {
 	    // start the playback of the background music immediately
 		rainMusic.setLooping(true);
 		rainMusic.play();
-		
-		
-		bucket = new Rectangle();
-	    bucket.x = 800 / 2 - 64 / 2;
-	    bucket.y = 20;
-	    bucket.width = 64;
-	    bucket.height = 64;
-		raindrops = new Array<Rectangle>();
-		spawnRaindrop();
 	}
 
 	@Override
@@ -72,16 +89,24 @@ public class tutorialGame extends Game {
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		
+		/*
+		 * the movement of the game. Users can press:
+		 * LEFT arrow key: to move left
+		 * RIGHT arrow key: to move right
+		 * D key: to accelerate
+		 */
+		
 		float deltaTime = Gdx.graphics.getDeltaTime();
 		if (Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= SPEED * deltaTime;
 		if (Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += SPEED * deltaTime;
 		
-		if (Gdx.input.isKeyPressed(Keys.D) && !activated) {System.out.println("cool"); powerActivated();} 
-		if(activated && TimeUtils.nanoTime() - lastPowerTime > 2_100_000_000) {System.out.println("Deactived??"); powerDeactivated();}
+		if (Gdx.input.isKeyPressed(Keys.D) && !activated) powerActivated(); 
+		if(activated && TimeUtils.nanoTime() - lastPowerTime > 2_100_000_000) powerDeactivated();
 		
 		
-		
+		// every interval spawn new raindrop or log		
 		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+		if(TimeUtils.nanoTime() - lastLogTime > 2000000000) spawnLog();
 		
 		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
 		      Rectangle raindrop = iter.next();
@@ -95,6 +120,19 @@ public class tutorialGame extends Game {
 		       }
 		}
 		
+		for (Iterator<Rectangle> iter = logs.iterator(); iter.hasNext(); ) {
+		      Rectangle log = iter.next();
+		      log.y -= 200 * Gdx.graphics.getDeltaTime();
+		      if(log.y + 64 < 0) iter.remove();
+		      
+		      if(log.overlaps(bucket)) {
+		    	  score--;
+		          dropSound.play();
+		          iter.remove();
+		       }
+		}
+		
+		// bound the bucket within the frame
 		if (bucket.x < 0) bucket.x = 0;
 		if (bucket.x > 725) bucket.x = 725;
 		
@@ -103,20 +141,38 @@ public class tutorialGame extends Game {
 		batch.begin();
 		
 		batch.draw(bucketImg, bucket.x, bucket.y);
+		
 		for(Rectangle raindrop: raindrops) {
 		      batch.draw(dropletImg, raindrop.x, raindrop.y);
 		}
 		
+		for(Rectangle log: logs) {
+		      batch.draw(logImg, log.x, log.y);
+		}
 		
 		font.draw(batch, Integer.toString(score), 10, 20);
 		
 		batch.end();
+		
+		// TESTING START
+//		shapeRenderer.begin(ShapeType.Line);
+//		shapeRenderer.setColor(1, 0, 0, 1); // Red line
+//		for(Rectangle raindrop: raindrops) {
+//			shapeRenderer.rect(raindrop.x, raindrop.y, raindrop.width, raindrop.height);
+//		}
+//		for(Rectangle log: logs) {
+//			shapeRenderer.rect(log.x, log.y, log.width, log.height);
+//		}
+//		
+//		shapeRenderer.end();
+//		// TESTING END
 	}
 	
 	@Override
     public void dispose() {
        dropletImg.dispose();
        bucketImg.dispose();
+       logImg.dispose();
        dropSound.dispose();
        rainMusic.dispose();
        batch.dispose();
@@ -133,8 +189,18 @@ public class tutorialGame extends Game {
 	      lastDropTime = TimeUtils.nanoTime();
    }
    
+   private void spawnLog() {
+	   Rectangle log = new Rectangle();
+	   log.x = MathUtils.random(0, 800-64);
+	   log.y = 480;
+	   log.width = 180;
+	   log.height = 80;
+	   logs.add(log);
+	   lastLogTime = TimeUtils.nanoTime();
+   }
+   
    private void powerActivated() {
-	   SPEED *= 2;
+	   SPEED *= 3;
 	   lastPowerTime = TimeUtils.nanoTime();
 	   activated = true;
    }
